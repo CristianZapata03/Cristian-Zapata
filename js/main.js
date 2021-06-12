ccb843551aabcef3c4d11bf8ca14329ab76f8876
Cristian Zapata <camilozapata22@hotmail.com>
2021-06-12 17:18:08 -0500

    inicio Cristian Zapata

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.onload = inicio;
+
+function inicio(){
+   /*var nombre = prompt("Ingrese su nommbre");
+   alert("Hola "+nombre  + " Sea Bienvenid@ y conoce un poco sobre mi");
+   console.log("Sea Bienvenido");*/
+}
